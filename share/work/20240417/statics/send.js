52c0b7158660bc6cfb3da0eb4e9257e955b03289
SMGoro <72185434+SMGoro@users.noreply.github.com>
2024-04-25 00:53:22 +0800

    Update send.js

--- share/work/20240417/statics/send.js
+++ share/work/20240417/statics/send.js
@@ -11,44 +11,8 @@ function getCurrentTime() {
     return `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
 }   
 
-// 按键发送
-textarea.addEventListener('keydown', function(event) {
-    if (event.key === 'Alt') {
-        textarea.value += '<br>'; // 处理换行
-    }
-    if (event.key === 'Enter') { // 
-        event.preventDefault(); // 阻止换行
-
-        // 发送消息的逻辑
-        const message = textarea.value;
-        if (message.trim() !== '') {
-            const messageDiv = document.createElement('div');
-            messageDiv.classList.add('message');
-            messageDiv.innerHTML = `
-            <div class="message-box right">
-                <span class="time right">${getCurrentTime()}</span>
-                <p class="text">
-                    ${message}
-                </p>
-            </div>
-            <span class="name right">热巴</span>
-            `;
-            chatArea.appendChild(messageDiv);
-            textarea.value = '';
-            console.log('发送消息:', message);
-        }
-    }
-});
-
-// 按钮发送
-sendBtn.addEventListener('click', function() {
-    const message = textarea.value; // 获取 textarea 里的内容
-
-    if (message.trim() === '') {
-        alert('不能发送空白消息');
-        return;
-    }
-
+function send() {
+    const message = textarea.value;
     // 在 chat-area 里添加 message div 展示发送内容
     const messageDiv = document.createElement('div');
     messageDiv.classList.add('message');
@@ -69,4 +33,35 @@ sendBtn.addEventListener('click', function() {
     // 在这里发送消息的逻辑
     console.log(getCurrentTime());
     console.log('发送消息:', message);
+
+    // 自动跳到聊天区域尾部
+    chatArea.scrollTop = chatArea.scrollHeight;
+}
+
+// 按键发送
+textarea.addEventListener('keydown', function(event) {
+    if (event.key === 'Alt') {
+        textarea.value += '<br>'; // 处理换行
+        return; // 阻止默认行为，避免输入 alt 字符
+    }
+    if (event.key === 'Enter') { // 
+        event.preventDefault(); // 阻止换行
+
+        // 发送消息的逻辑
+        const message = textarea.value;
+        if (message.trim() !== '') {
+            send.apply();
+        }
+    }
+});
+
+// 按钮发送
+sendBtn.addEventListener('click', function() {
+    const message = textarea.value; // 获取 textarea 里的内容
+
+    if (message.trim() === '') {
+        alert('不能发送空白消息');
+        return;
+    }
+    send.apply();
 });
